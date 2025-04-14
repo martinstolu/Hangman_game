44f2a8c7cdedbde06b9531866239ed943ae9c69e
Tolu Martins <tolu.martins.a@gmail.com>
2025-04-15 01:23:26 +0200

    hangman.py
    Added main Hangman game file

--- hangman.py
+++ hangman.py
@@ -1,10 +1,110 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
-print(f"Each person should pay: ${final_amount}")
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel", "elephant", "giraffe", "kangaroo",
+             "dolphin", "penguin", "rhinoceros", "flamingo", "alligator",
+             "chimpanzee", "porcupine", "crocodile", "meerkat", "zebra",
+             "hippopotamus", "lemur", "wombat", "platypus", "toucan",
+             "koala", "cheetah", "jaguar", "panther", "ostrich",
+             "antelope", "mongoose", "sloth", "armadillo"]
+
+chosen_word = random.choice(word_list)
+lives = 6
+# print(chosen_word)
+all_guessed_letters = []
+correct_letters = []
+
+game_over = False
+while not game_over:
+    print(stages[lives])
+    print(f"You've got {lives} tries left.")
+
+    placeholder = ""
+
+    for letter in chosen_word:
+        if letter in correct_letters:
+            placeholder += letter
+        else:
+            placeholder += "_"
+    print("Word: " + placeholder)
+
+    guess = input("Guess a letter: \n").lower()
+
+    while guess in all_guessed_letters:
+        print("you've guessed this letter already")
+        print("Word: " + placeholder)
+        print(stages[lives])
+        print(f"You've got {lives} tries left.")
+        guess = input("Guess a letter: \n").lower()
+
+    all_guessed_letters.append(guess)
+    if guess in chosen_word:
+        correct_letters.append(guess)
+        print("Nice! You got a letter.")
+    else:
+        print("Ouch! Wrong letter.")
+        lives -= 1
+
+    if "_" not in placeholder:
+        print("You won! The word was:", chosen_word)
+        game_over = True
+
+    if lives == 0:
+        print(stages[lives])
+        print("You died - game over")
+        print("The correct word was:", chosen_word)
+        game_over = True
